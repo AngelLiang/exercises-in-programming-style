75600ec789ff6a1c4b0f938e694818daac6944ef
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2019-12-19 09:19:42 +0800

    Added notes

--- 10-things/tf-10.py
+++ 10-things/tf-10.py
@@ -13,8 +13,14 @@ class TFExercise():
 
 class DataStorageManager(TFExercise):
     """ Models the contents of the file """
-    
+
     def __init__(self, path_to_file):
+        """
+        从程序外部获取文本数据，并将其转化为能被应用程序其他部分所使用的单词。
+        本类是用来处理与问题文本数据输入相关的数据和行为的抽象。
+
+        :param path_to_file: str, 文件路径
+        """
         with open(path_to_file) as f:
             self._data = f.read()
         pattern = re.compile('[\W_]+')
@@ -29,14 +35,17 @@ class DataStorageManager(TFExercise):
 
 class StopWordManager(TFExercise):
     """ Models the stop word filter """
-    
+
     def __init__(self):
+        """向应用程序其他部分提供鉴别停止词的服务。"""
+        # 获取停止词
         with open('../stop_words.txt') as f:
             self._stop_words = f.read().split(',')
         # add single-letter words
         self._stop_words.extend(list(string.ascii_lowercase))
 
     def is_stop_word(self, word):
+        """鉴别停止词"""
         return word in self._stop_words
 
     def info(self):
@@ -46,6 +55,7 @@ class WordFrequencyManager(TFExercise):
     """ Keeps the word frequency data """
     
     def __init__(self):
+        """用于管理单词技术，其内部使用了字典来实现单词与计数的映射。"""
         self._word_freqs = {}
 
     def increment_count(self, word):
@@ -61,12 +71,16 @@ class WordFrequencyManager(TFExercise):
         return super(WordFrequencyManager, self).info() + ": My major data structure is a " + self._word_freqs.__class__.__name__
 
 class WordFrequencyController(TFExercise):
+    """程序的入口"""
+
     def __init__(self, path_to_file):
+        # 组合各种 Manager
         self._storage_manager = DataStorageManager(path_to_file)
         self._stop_word_manager = StopWordManager()
         self._word_freq_manager = WordFrequencyManager()
 
     def run(self):
+        """主方法"""
         for w in self._storage_manager.words():
             if not self._stop_word_manager.is_stop_word(w):
                 self._word_freq_manager.increment_count(w)
